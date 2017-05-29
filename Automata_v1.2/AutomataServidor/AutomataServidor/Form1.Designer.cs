42251c097ef950c3337bfacb731a00f099e5694a
Marco Abarca <marqubit@gmail.com>
2017-05-28 22:15:54 -0500

    Automata_Parcial2.sln

---
+++ Automata_v1.2/AutomataServidor/AutomataServidor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace AutomataServidor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(12, 85);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(432, 148);
+            this.listBox1.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(143, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 26);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "1234";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "inicio";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(12, 252);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            this.chart1.Size = new System.Drawing.Size(432, 275);
+            this.chart1.TabIndex = 7;
+            this.chart1.Text = "chart1";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(355, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 29);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "graficar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(460, 539);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Servidor";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Automata_v1.2/AutomataServidor/AutomataServidor/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Windows.Forms.DataVisualization.Charting;
+
+
+namespace AutomataServidor
+{
+    public partial class Form1 : Form
+    {
+        int[] contarN = new int[2];//numero de clientes que admite
+        int[] contarS = new int[2];
+        string[] clientes = new string[2];
+        int cont = 0;
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> clientSockets = new List<Socket>();
+        byte[] buffer = new byte[2048];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }       
+        
+        public void AceptarPeticion(IAsyncResult estadoAsincrono)
+        {
+            CheckForIllegalCrossThreadCalls = false;//Desactiva la comprobacion de acceso de varios subprocesos
+            Socket cliente;
+
+            try
+            {
+                cliente = serverSocket.EndAccept(estadoAsincrono);
+            }
+            catch (ObjectDisposedException)
+            {
+                return;
+            }
+            clientSockets.Add(cliente);
+            cliente.BeginReceive(buffer, 0, 2048, 0, ProcesarMensaje, cliente);
+            listBox1.Items.Add("Se ha conectado un cliente");
+
+            serverSocket.BeginAccept(AceptarPeticion, null);
+        }
+
+        public void ProcesarMensaje(IAsyncResult AR)
+        {
+
+            GenerarCadenas(); 
+            string cadenas = "";
+            using (StreamReader leerArchivo = new StreamReader("cadenas.txt"))
+            {
+                string line;
+                while ((line = leerArchivo.ReadLine()) != null)
+                {
+                    cadenas += line;
+                }
+            }
+
+            Socket current = (Socket)AR.AsyncState;
+            int received;
+
+            try
+            {
+                received = current.EndReceive(AR);
+            }
+            catch (SocketException)
+            {
+                listBox1.Items.Add("Se ha desconectado el cliente");
+                current.Close();
+                clientSockets.Remove(current);
+                return;
+            }
+
+            byte[] mensaje = new byte[received];
+            Array.Copy(buffer, mensaje, received);
+            string host = Encoding.ASCII.GetString(mensaje);
+            string[] host1 = host.Split(',');
+            
+            listBox1.Items.Add("El cliente es : " + host1[0]);
+
+            if(host.Contains(",") == true )
+            {
+                //no se porque marca error
+                //pero que lo que se tiene que hacer es que agrege 
+                //el nombre de los clientes a un array para utlizarlos en la grafica
+                //clientes[cont] = host1[0].ToString();
+                contarS[cont] = Int32.Parse(host1[1]);
+                contarN[cont] = Int32.Parse(host1[2]);
+                cont++;
+            }
+            
+            if (host != "")
+            {
+                listBox1.Items.Add("Enviando cadenas al cliente: " + host1[0]);
+                byte[] cadena = Encoding.ASCII.GetBytes(cadenas);
+                current.Send(cadena);
+            }
+
+            current.BeginReceive(buffer, 0, 2048, 0, ProcesarMensaje, current);
+        }
+
+        public void GenerarCadenas()
+        {
+            StreamWriter escribirCadena = new StreamWriter("cadenas.txt");
+            Random cantidadCadenas = new Random();
+            Random elemnetosCadena = new Random();
+            Random tamanoCadena = new Random();
+
+            int cantidad = cantidadCadenas.Next(2, 100);
+
+            for (int i = 0; i < cantidad; i++)
+            {
+                int tamano = tamanoCadena.Next(2, 20);
+                for (int j = 0; j < tamano; j++)
+                {
+                    escribirCadena.Write(elemnetosCadena.Next(2));
+                }
+                escribirCadena.WriteLine("&");
+            }
+            escribirCadena.Close();
+        }        
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            listBox1.Items.Add("Servidor Corriendo");
+            serverSocket.Bind(new IPEndPoint(IPAddress.Any, int.Parse(textBox1.Text)));
+            serverSocket.Listen(0);
+            serverSocket.BeginAccept(AceptarPeticion, null);
+        }
+
+        public void Graficar()
+        {
+            String[] datos = { "Validos", "Invalidos" };
+
+            chart1.Titles.Add("Resultado de cadenas evaluadas por clientes");
+            
+            Series serie = chart1.Series.Add("Validos");
+            Series serie1 = chart1.Series.Add("inValidos");
+            for (int i = 0; i < contarS.Length; i++)
+            {
+                //en serie.label tendria que indicar el numero que correspodiente
+                //pero en todas utliza el ultimo elemento que entra
+                //se tiene que areglar
+                serie.Label = contarS[i].ToString();
+                serie1.Label = contarN[i].ToString();                
+                //chart1.Series["Validos"].Points[i+1].AxisLabel = "First";
+                serie.Points.Add(contarS[i]);
+                serie1.Points.Add(contarN[i]);
+            }
+
+            //aun no se como poner el nombre de los clientes en la grafica
+            serie.Points[1].AxisLabel = "yo";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Graficar();
+        }
+    }
+}
